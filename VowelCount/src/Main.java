286b9f26d3b059a08d8f769f7bf4738e03922cd5
Yasamin <yasamin.fazliani@senecapolytechnic.ca>
2025-09-17 18:20:26 -0400

    First commit.

---
+++ VowelCount/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static int countAllVowels(String input){
+        int count = 0;
+
+        for (int i = 1; i < input.length(); i++){
+            char c = input.charAt(i);
+            switch(c) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(countAllVowels("Empty"));
+    }
+}
